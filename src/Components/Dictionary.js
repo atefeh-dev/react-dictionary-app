ab66f1bdc4062e6674af834242be939a966afb68
atefehmohammaddoust <atefeh.mohammaddoust@gmail.com>
2021-09-24 07:38:48 +0330

    ADD FOOTER

--- src/Components/Dictionary.js
+++ src/Components/Dictionary.js
@@ -2,12 +2,14 @@
 
 import React from "react";
 import Search from "./Search";
+import Footer from "./Footer";
 import "../Style/Dictionary.css";
 
 const Dictionary = () => {
   return (
     <div className="Dictionary">
       <Search />
+      <Footer />
     </div>
   );
 };
